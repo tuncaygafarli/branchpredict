18a432d367b7bf5d9e09c99267c6553f70a4efe1
Tuncay <68643463+tuncaygafarli@users.noreply.github.com>
2025-12-26 23:47:17 +0400

    Fixed CPU auto execution handler

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <string>
 #include <iostream>
+#include <thread>
+#include <chrono>
 
 #include <SFML/Graphics.hpp>
 #include "gui/gui_render.h"
@@ -11,8 +13,8 @@
 
 int main(int argc, char** argv) {
     std::string input_file;
-    std::string log_destination = "none"; 
-    CPU::PREDICTOR_TYPE predictor_type = CPU::PREDICTOR_TYPE::GSHARE; 
+    std::string log_destination = "none";
+    CPU::PREDICTOR_TYPE predictor_type = CPU::PREDICTOR_TYPE::GSHARE;
 
     // GUI TEST
     GUIRender gui_render;
@@ -26,25 +28,33 @@ int main(int argc, char** argv) {
         if (arg == "--log") {
             if (i + 1 < argc) {
                 log_destination = argv[i + 1];
-                i++; 
-            } else {
+                i++;
+            }
+            else {
                 std::cerr << "Error: --log requires a destination (cout or a filename).\n";
                 return 1;
             }
-        } else if (arg == "--gshare") {
+        }
+        else if (arg == "--gshare") {
             predictor_type = CPU::PREDICTOR_TYPE::GSHARE;
-        } else if (arg == "--GAg") {
+        }
+        else if (arg == "--GAg") {
             predictor_type = CPU::PREDICTOR_TYPE::GAg;
-        } else if (arg == "--PAg") {
+        }
+        else if (arg == "--PAg") {
             predictor_type = CPU::PREDICTOR_TYPE::PAg;
-        } else if (arg == "--simple") {
+        }
+        else if (arg == "--simple") {
             predictor_type = CPU::PREDICTOR_TYPE::SIMPLE;
-        } else if (arg.rfind("--", 0) == 0) { // Checks if the argument starts with '--'
+        }
+        else if (arg.rfind("--", 0) == 0) { // Checks if the argument starts with '--'
             std::cerr << "Warning: Unknown option: " << arg << ". Ignoring.\n";
-        } else {
+        }
+        else {
             if (input_file.empty()) {
                 input_file = arg;
-            } else {
+            }
+            else {
                 std::cerr << "Warning: Multiple input files found. Using first one: " << input_file << ".\n";
             }
         }
@@ -56,82 +66,98 @@ int main(int argc, char** argv) {
     }
 
     std::ofstream log_file;
-    std::ostream* log_stream = nullptr;  
-	if (log_destination == "cout") {
+    std::ostream* log_stream = nullptr;
+    if (log_destination == "cout") {
         log_stream = &std::cout;
-    } else if (log_destination != "none") {
+    }
+    else if (log_destination != "none") {
         log_file.open(log_destination);
         if (!log_file.is_open()) {
             std::cerr << "Error: Could not open log file '" << log_destination << "'\n";
             return 1;
         }
         log_stream = &log_file;
-    } 
+    }
 
     parser_t parser;
-    CPU cpu(predictor_type); 
+    CPU cpu(predictor_type);
 
-    cpu.load_program(parser.parse_program(input_file, gui_render)); 
+    cpu.load_program(parser.parse_program(input_file, gui_render));
     gui_render.init(cpu);
     bool cpu_halted = false;
+    bool autorun = false;
 
-    /*
-    while (!cpu.halt()) {
-        cpu.execute();
-    }
-    */
-    
-    
     if (log_stream) {
-        cpu.log(*log_stream);  
+        cpu.log(*log_stream);
     }
+
+    sf::Clock autorun_timer;
+    const float autorun_delay = 0.5f;
 
     while (window.isOpen())
     {
+        float delta_time = autorun_timer.restart().asSeconds();
+        static float accumulator = 0.f;
+
         sf::Event event;
+
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-                selection_index++;
-                if (selection_index >= gui_render.instruction_elements.size()) {
-                    selection_index = 0;
-                }
-                gui_render.set_selection(selection_index);
-                gui_render.ensure_visible(selection_index);
-            }
 
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-                selection_index--;
-
-                if (selection_index < 0) {
-                    selection_index = gui_render.instruction_elements.size() - 1;
-                }
-
-                gui_render.set_selection(selection_index);
-                gui_render.ensure_visible(selection_index);
-            }
-
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::PageDown)) {
-                gui_render.scroll(100.f);  // Scroll down
-            }
-
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::PageUp)) {
-                gui_render.scroll(-100.f); // Scroll up
-            }
-
-            if (event.key.code == sf::Keyboard::Space) {
-                if (!cpu_halted) {
-                    cpu.execute();
-                    cpu_halted = cpu.halt();
-
-                    gui_render.update_registers(cpu);
-
-                    selection_index = cpu.get_pc();
+            if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::Down) {
+                    selection_index++;
+                    if (selection_index >= gui_render.instruction_elements.size()) {
+                        selection_index = 0;
+                    }
                     gui_render.set_selection(selection_index);
                     gui_render.ensure_visible(selection_index);
                 }
+
+                if (event.key.code == sf::Keyboard::Up) {
+                    selection_index--;
+                    if (selection_index < 0) {
+                        selection_index = gui_render.instruction_elements.size() - 1;
+                    }
+                    gui_render.set_selection(selection_index);
+                    gui_render.ensure_visible(selection_index);
+                }
+
+                if (event.key.code == sf::Keyboard::Space) {
+                    if (!cpu_halted) {
+                        cpu.execute();
+                        cpu_halted = cpu.halt();
+
+                        gui_render.update_registers(cpu);
+
+                        selection_index = cpu.get_pc();
+                        gui_render.set_selection(selection_index);
+                        gui_render.ensure_visible(selection_index);
+                    }
+                }
+
+                if (event.key.code == sf::Keyboard::R) {
+                    autorun = !autorun;
+                    accumulator = 0.f;
+                }
+            }
+        }
+
+        if (autorun && !cpu_halted) {
+            accumulator += delta_time;
+
+            while (accumulator >= autorun_delay && !cpu_halted) {
+                accumulator -= autorun_delay;
+
+                cpu.execute();
+                cpu_halted = cpu.halt();
+
+                gui_render.update_registers(cpu);
+                selection_index = cpu.get_pc();
+                gui_render.set_selection(selection_index);
+                gui_render.ensure_visible(selection_index);
             }
         }
 
